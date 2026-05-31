a09c69a95a63c4671c0c3920249009deec9add46
maksymostapiak <maksym.ostapiak.23@pnu.edu.ua>
2026-05-31 11:11:04 +0000

    fixed

--- models/database.py
+++ models/database.py
@@ -2,7 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/library_db")
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+SQLITE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'library.db')}"
+
+DATABASE_URL = os.getenv("DATABASE_URL", SQLITE_URL)
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
